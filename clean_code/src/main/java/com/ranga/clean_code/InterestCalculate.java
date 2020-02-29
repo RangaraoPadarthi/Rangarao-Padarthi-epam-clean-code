18a511a9ecda53662539758dca85d0472c971e6c
RangaraoPadarthi <17pa1a05b9@vishnu.edu.in>
2020-02-29 14:39:02 +0530

    commit

---
+++ clean_code/src/main/java/com/ranga/clean_code/InterestCalculate.java
@@ -0,0 +1,27 @@
+package com.ranga.clean_code;
+
+public class InterestCalculate {
+
+	public double simpleInterest(double principal,double time,double rateOfInterest){
+
+        double simpleinterest = principal*time*rateOfInterest/100;
+
+        return simpleinterest;
+
+    }
+
+
+
+    public double compoundInterest(double principal,double time,double rateOfInterest,double num){
+
+        double amount = principal * Math.pow(1 + (rateOfInterest / num), num * time);
+
+        double compoundinterest = amount - principal;
+
+        return compoundinterest;
+
+    }
+
+
+
+}
